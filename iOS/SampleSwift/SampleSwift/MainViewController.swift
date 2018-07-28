6dbe05f229f814110129b02bdceb03ef15c6bd05
Andrew Robinson <arob@yelp.com>
2018-07-27 17:52:25 -0700

    Allow SampleSwift to launch

---
+++ iOS/SampleSwift/SampleSwift/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  SampleSwift
+//
+//  Created by Andrew Robinson on 7/27/18.
+//  Copyright © 2018 Noah Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tappedNetworkInspector(_ sender: UIButton) {
+        // Currently not supported!
+//        let storyboard = UIStoryboard(name: "MainStoryBoard", bundle: nil)
+//        let networkViewController = storyboard.instantiateViewController(withIdentifier: "NetworkViewController")
+//        self.navigationController?.pushViewController(networkViewController, animated: true)
+    }
+
+}
